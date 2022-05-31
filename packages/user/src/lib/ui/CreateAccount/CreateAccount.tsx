6af4ad5ace5ca7e5aab626fd20afab84e0b6478e
AdaoBJr <adaobjr@gmail.com>
2022-05-30 20:58:09 -0400

    feat: :sparkles: created @dash/user

---
+++ packages/user/src/lib/ui/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { SxProps, TextField, Theme } from '@mui/material';
+import { ReactFC } from 'types/interface';
+
+interface CreateAccountProps extends ReactFC {
+  sx?: SxProps<Theme>;
+}
+
+export const CreateAccount: React.FC<CreateAccountProps> = ({ sx }) => {
+  const response = { warning: false, loading: false };
+  return (
+    <>
+      <TextField
+        id="firstname"
+        name="firstname"
+        sx={{ ...sx }}
+        variant="outlined"
+        error={response.warning}
+        inputProps={{
+          minLength: 3,
+          // value: values.user,
+          // onChange: handleChange,
+          // onKeyDown: handleOnKeyDown,
+        }}
+      />
+      <TextField
+        id="lastname"
+        name="lastname"
+        sx={{ ...sx }}
+        variant="outlined"
+        error={response.warning}
+        inputProps={{
+          minLength: 3,
+          // value: values.user,
+          // onChange: handleChange,
+          // onKeyDown: handleOnKeyDown,
+        }}
+      />
+    </>
+  );
+};
---
+++ packages/user/src/lib/ui/Login/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactFC } from 'types/interface';
+
+export const Login: React.FC<ReactFC> = () => {
+  return (
+    <>
+      <h1>LOGIN</h1>
+    </>
+  );
+};
