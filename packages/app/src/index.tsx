d95d725bfe7170e0f074071e349357b7b9af041c
AdaoBJr <adaobjr@gmail.com>
2022-06-05 16:52:36 -0400

    chore: :art: updated index.tsx

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { makeServer, queryClient } from 'services/infra';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+if (process.env.REACT_APP_NODE_ENV !== 'production') makeServer();
+
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+        <ReactQueryDevtools />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
