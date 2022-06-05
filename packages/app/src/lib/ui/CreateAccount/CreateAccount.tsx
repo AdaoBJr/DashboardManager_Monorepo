879d98cf40a7817281ca2096d0ad081e06e87545
AdaoBJr <adaobjr@gmail.com>
2022-06-05 17:01:22 -0400

    feat: :sparkles: created inittial connection with react-query / miragejs

--- packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
+++ packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { ReactFC } from 'types/interface';
+import { useQuery } from 'react-query';
+import { Customer, ReactFC } from 'types/interface';
 import { CreateAccountModule } from '@dash/module-customer';
 
+import { useRestRequest } from 'services/hooks';
+
 export const CreateAccount: React.FC<ReactFC> = () => {
+  const request = useRestRequest();
+  const { data, isLoading } = useQuery<Customer[]>('customers', async () => {
+    const { data } = await request.get('customers');
+    return data.customers;
+  });
+
+  console.log('data', data);
+
   return (
     <div style={{ display: 'grid', placeItems: 'center' }}>
       <CreateAccountModule />
