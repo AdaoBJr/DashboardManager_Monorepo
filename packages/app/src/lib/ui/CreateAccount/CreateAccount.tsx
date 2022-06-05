3db00cb7763c468d74a701fa7259fc4530f78e49
AdaoBJr <adaobjr@gmail.com>
2022-06-05 19:46:53 -0400

    feat: :boom: used graphql with axios and react-query on CreateAccount

--- packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
+++ packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
@@ -3,16 +3,32 @@ import { useQuery } from 'react-query';
 import { Customer, ReactFC } from 'types/interface';
 import { CreateAccountModule } from '@dash/module-customer';
 
-import { useRestRequest } from 'services/hooks';
+import { useRequest } from 'services/hooks';
+import { createCustomerGQL } from 'gql/customer';
 
 export const CreateAccount: React.FC<ReactFC> = () => {
-  const request = useRestRequest();
-  const { data, isLoading } = useQuery<Customer[]>('customers', async () => {
-    const { data } = await request.get('customers');
-    return data.customers;
+  const request = useRequest();
+
+  const data = {
+    firstname: 'Adam1',
+    lastname: 'Jr',
+    password: '1234',
+    email: 'adam1@mail.com',
+    dateOfBirth: '25/07/1990',
+    gender: 1,
+    country: 'BR',
+    cpf: '028905361-73',
+    telephone: '99225-2201',
+    subscribe: false
+  };
+  const { query, variables } = createCustomerGQL({ data });
+
+  const { data: response } = useQuery<Customer[]>('customers', async () => {
+    const response = await request.graphql('customers', { query, variables });
+    return response.data.customer.variables.v1;
   });
 
-  console.log('data', data);
+  console.log('response', response);
 
   return (
     <div style={{ display: 'grid', placeItems: 'center' }}>
