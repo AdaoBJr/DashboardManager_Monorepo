6205c1a09bdf38084ca9a1b80cdabcfbeac55d29
AdaoBJr <adaobjr@gmail.com>
2022-06-06 01:26:22 -0400

    refactor: :sparkles: created queryRequest on Utils

---
+++ packages/app/src/services/utils/query/index.ts
@@ -0,0 +1,15 @@
+import { useRequest } from 'services/hooks';
+import { Gql } from 'types/interface';
+
+const request = useRequest();
+
+interface QueryResquestProps {
+  route: string;
+  gql: Gql;
+  db: string;
+}
+
+export const queryResquest = async ({ route, gql, db }: QueryResquestProps) => {
+  const response = await request.graphql(route, gql);
+  return response.data?.[db].variables.v1;
+};
