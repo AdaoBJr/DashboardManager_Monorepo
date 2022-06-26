19f73ae1cfef3a7d4669f5cb4d056f7e400d3270
AdaoBJr <adaobjr@gmail.com>
2022-06-25 20:06:43 -0400

    feat: :boom: added Logic and Inputs on SignInModule

--- packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
+++ packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import { ReactFC } from 'types/interface';
+import React, { ChangeEvent, useCallback, useState } from 'react';
+import { Grid, GridProps, TextField, TextFieldProps } from '@mui/material';
 
-export const SignInModule: React.FC<ReactFC> = () => {
+import { ReactFC } from 'types/interface';
+import { SignInDomain } from '../../../types/domain';
+
+interface SignInProps extends ReactFC {
+  container?: GridProps;
+  item?: GridProps;
+  input?: TextFieldProps;
+  onBlur?: (values: SignInDomain) => void;
+}
+
+export const SignInModule: React.FC<SignInProps> = ({
+  container,
+  item,
+  input,
+  onBlur
+}) => {
+  const [values, setValues] = useState({} as SignInDomain);
+
+  const handleChange = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) =>
+      setValues({ ...values, [target.name]: target.value }),
+    [setValues]
+  );
+
   return (
-    <>
-      <h1>LOGIN</h1>
-    </>
+    <Grid container {...container}>
+      <Grid item xs={12} {...item}>
+        <TextField
+          id="email"
+          name="email"
+          label="Nome"
+          variant={input?.variant || 'outlined'}
+          onChange={handleChange}
+          onBlur={() => onBlur?.({ ...values })}
+          {...input}
+        />
+      </Grid>
+      <Grid item xs={12} {...item}>
+        <TextField
+          id="password"
+          name="password"
+          label="Nome"
+          variant={input?.variant || 'outlined'}
+          onChange={handleChange}
+          onBlur={() => onBlur?.({ ...values })}
+          {...input}
+        />
+      </Grid>
+    </Grid>
   );
 };
