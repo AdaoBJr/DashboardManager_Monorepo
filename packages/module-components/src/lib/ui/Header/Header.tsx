1820f0f1eb18c9067a6467333b66db07e207e2af
AdaoBJr <adaobjr@gmail.com>
2022-06-26 12:03:18 -0400

    feat: :sparkles: created HeaderModule on module-components

--- packages/module-components/src/lib/ui/Header/Header.tsx
+++ packages/module-components/src/lib/ui/Header/Header.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import { Box, SxProps, Theme } from '@mui/material';
+
+import { Image } from 'lib/shared';
 import { ReactFC } from 'types/interface';
 
-export const HeaderModule: React.FC<ReactFC> = () => {
-  return <div>HeaderModule</div>;
-};
+interface HeaderModuleProps extends ReactFC {
+  fixed?: boolean;
+  src: string;
+  justify?: string;
+  heightHeader?: string;
+  widthImg?: string;
+  heightImg?: string;
+  altImg?: string;
+  sx?: SxProps<Theme>;
+}
+
+export const HeaderModule: React.FC<HeaderModuleProps> = props => (
+  <Box
+    sx={{
+      position: props.fixed ? 'fixed' : 'inherit',
+      display: 'flex',
+      alignItems: 'flex-end',
+      justifyContent: {
+        xs: props.justify || 'center',
+        md: props.justify || 'start'
+      },
+      marginTop: { xs: '30px', md: '0' },
+      ...props.sx
+    }}
+  >
+    <Image
+      src={props.src}
+      height={props.heightImg || 50}
+      width={props.widthImg || 170}
+      alt={props.altImg || 'logo GoDash'}
+    />
+  </Box>
+);
