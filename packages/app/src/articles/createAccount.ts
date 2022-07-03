29231314646251717f4c8b8f1c620b67d29289e2
AdaoBJr <adaobjr@gmail.com>
2022-07-03 15:34:07 -0400

    feat: :sparkles: created articles on App/CreateAccount

---
+++ packages/app/src/articles/createAccount.ts
@@ -0,0 +1,25 @@
+export const createAccount = [
+  { xs: 12, sm: undefined, id: 'firstname', name: 'firstname', label: 'Nome' },
+  { xs: 12, sm: undefined, id: 'lastname', name: 'lastname', label: 'Sobrenome' },
+  { xs: 12, sm: undefined, id: 'email', name: 'email', label: 'E-mail' },
+  {
+    xs: 12,
+    sm: 6,
+    id: 'dateOfBirth',
+    name: 'dateOfBirth',
+    label: 'Data de aniversário'
+  },
+  { xs: 12, sm: 6, id: 'gender', name: 'gender', label: 'Gênero' },
+  { xs: 12, sm: 6, id: 'cpf', name: 'cpf', label: 'CPF' },
+  { xs: 12, sm: 6, id: 'country', name: 'country', label: 'País' },
+  { xs: 12, sm: 6, id: 'telephone', name: 'telephone', label: 'Telefone' },
+  { xs: 12, sm: 6, id: 'subscribe', name: 'subscribe', label: 'Se Inscreva' },
+  { xs: 12, sm: undefined, id: 'password', name: 'password', label: 'Senha' },
+  {
+    xs: 12,
+    sm: undefined,
+    id: 'password_confirm',
+    name: 'password_confirm',
+    label: 'Confirme a senha'
+  }
+];
