8ced652c1bc7d19b7213924894c6f074722ada85
AdaoBJr <adaobjr@gmail.com>
2022-07-05 20:48:14 -0400

    feat: :label: created links interfaces in HeaderModule on Types

--- packages/module-components/src/types/interface/Header/HeaderModuleProps.ts
+++ packages/module-components/src/types/interface/Header/HeaderModuleProps.ts
@@ -1,7 +1,20 @@
 import { ReactFC } from '../React';
 import { SxProps, Theme } from '@mui/material';
 
+interface LinkDetail {
+  name: string;
+  link: string;
+}
+
+interface Links {
+  link1?: LinkDetail;
+  link2?: LinkDetail;
+  link3?: LinkDetail;
+}
+
 export interface HeaderModuleProps extends ReactFC {
+  title?: string;
+  links?: Links;
   fixed?: boolean;
   src?: string;
   justify?: string;
