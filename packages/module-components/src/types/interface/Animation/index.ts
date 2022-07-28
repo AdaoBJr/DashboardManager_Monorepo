9c0e08b89107749cbb8e46201dc7ae9fd05ba5cb
AdaoBJr <adaobjr@gmail.com>
2022-07-28 09:55:13 -0400

    refactor: :sparkles: created type AnimationModuleProps on interface

---
+++ packages/module-components/src/types/interface/Animation/index.ts
@@ -0,0 +1,5 @@
+export interface AnimationModuleProps {
+  animation: any;
+  width?: number | string;
+  height?: number | string;
+}
--- packages/module-components/src/types/interface/index.ts
+++ packages/module-components/src/types/interface/index.ts
@@ -1,2 +1,3 @@
 export * from './React';
 export * from './Header';
+export * from './Animation';
