fdc1686726050e18f82a3394d499ca6ad6e7ea1c
AdaoBJr <adaobjr@gmail.com>
2022-07-28 16:24:17 -0400

    feat: :art: updated articles SignIn

--- packages/app/src/articles/signIn.ts
+++ packages/app/src/articles/signIn.ts
@@ -1,4 +1,6 @@
-export const signIn = [
+export const signInTitle = { xs: 12, sm: undefined, id: 'title', name: 'Login' };
+
+export const signInInputs = [
   { xs: 12, sm: undefined, id: 'email', name: 'email', label: 'E-mail' },
   { xs: 12, sm: undefined, id: 'password', name: 'password', label: 'Senha' }
 ];
