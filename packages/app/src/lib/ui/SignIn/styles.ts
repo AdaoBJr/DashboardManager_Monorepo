32aa9aba2233edff1f7e950d986ce88381e9b449
AdaoBJr <adaobjr@gmail.com>
2022-07-27 22:28:38 -0400

    style: :lipstick: updated styles in Header on SignIn

--- packages/app/src/lib/ui/SignIn/styles.ts
+++ packages/app/src/lib/ui/SignIn/styles.ts
@@ -4,13 +4,18 @@ import { Box, Paper, SxProps, Theme } from '@mui/material';
 export const BoxContainer = styled(Box, {
   name: 'SignIn',
   slot: 'container'
-})(({ theme }) => ({}));
+})(({ theme }) => ({
+  padding: '.25rem 1rem'
+}));
 
 export const PaperWrapper = styled(Paper, {
   name: 'SignIn',
   slot: 'header-wrapper'
 })(({ theme }) => ({
-  padding: '.25rem'
+  padding: '.25rem',
+  boxShadow: 'none',
+  filter:
+    'drop-shadow(0px 7px 2px rgba(0, 0, 0, 0.14)) drop-shadow(0px 6px 10px rgba(0, 0, 0, 0.12)) drop-shadow(0px 6px 10px rgba(0, 0, 0, 0.2))'
 }));
 
 export const LinkContainer: SxProps<Theme> = {
@@ -21,8 +26,13 @@ export const LinkContainer: SxProps<Theme> = {
 
 export const LinkItem: SxProps<Theme> = {
   background: theme => theme.palette.primary.main,
-  '&:hover': { background: theme => theme.palette.primary.dark },
   borderRadius: theme => theme.shape.borderRadius,
-  padding: '.4rem',
-  margin: '5px .5rem'
+  padding: '.3rem 1rem',
+  margin: '5px .5rem',
+  boxShadow: theme => theme.shadows[5],
+  '&:hover': {
+    background: theme => theme.palette.primary.dark,
+    color: '#fff',
+    transition: '.5s'
+  }
 };
