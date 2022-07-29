206fbc5192ce2584f243e83b666990c3cfcb27db
AdaoBJr <adaobjr@gmail.com>
2022-07-28 21:05:51 -0400

    feat: :sparkles: added icon burgerMenu on HeaderModule

--- packages/module-components/src/lib/ui/Header/Header.tsx
+++ packages/module-components/src/lib/ui/Header/Header.tsx
@@ -1,39 +1,48 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Icon, IconButton, Typography, useMediaQuery } from '@mui/material';
 
 import { Links } from './components';
 import { Image } from '../../../lib/shared';
 import { HeaderModuleProps } from '../../../types/interface';
 
-export const HeaderModule: React.FC<HeaderModuleProps> = props => (
-  <Box
-    sx={{
-      position: props.fixed ? 'fixed' : 'inherit',
-      width: '100%',
-      padding: '0 2rem',
-      ...props.sx
-    }}
-  >
+export const HeaderModule: React.FC<HeaderModuleProps> = props => {
+  const smDown = props?.theme && useMediaQuery(props?.theme.breakpoints.down('sm'));
+
+  return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: {
-          xs: props.justify || 'center',
-          md: props.justify || 'space-between'
-        }
+        position: props.fixed ? 'fixed' : 'inherit',
+        width: '100%',
+        padding: '0 2rem',
+        ...props.sx
       }}
     >
-      <Box>
-        <Image
-          src={props.src!}
-          height={props.heightImg}
-          width={props.widthImg || 70}
-          alt={props.altImg || 'logo GoDash'}
-        />
-        <Typography component="span">{props.title || 'GoDash'}</Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: {
+            xs: props.justify || 'center',
+            md: props.justify || 'space-between'
+          }
+        }}
+      >
+        <Box>
+          <Image
+            src={props.src!}
+            height={props.heightImg}
+            width={props.widthImg || 70}
+            alt={props.altImg || 'logo GoDash'}
+          />
+          <Typography component="span">{props.title || 'GoDash'}</Typography>
+        </Box>
+        {!smDown && props?.linksProps && <Links {...props?.linksProps} />}
+        {smDown && (
+          <IconButton>
+            <Icon>menu</Icon>
+          </IconButton>
+        )}
       </Box>
-      {props?.linksProps && <Links {...props?.linksProps} />}
     </Box>
-  </Box>
-);
+  );
+};
