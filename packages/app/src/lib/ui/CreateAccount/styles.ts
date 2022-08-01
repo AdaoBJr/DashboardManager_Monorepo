721ff46689017ec3795160235c6a9fbc4055691e
AdaoBJr <adaobjr@gmail.com>
2022-08-01 17:24:42 -0400

    style: :lipstick: created styles on CreateAccount

--- packages/app/src/lib/ui/CreateAccount/styles.ts
+++ packages/app/src/lib/ui/CreateAccount/styles.ts
@@ -1,1 +1,21 @@
-export {};
+import { SxProps, Theme } from '@mui/material';
+
+export const PaperForm: SxProps<Theme> = {
+  width: { xs: '100%', md: '500px' },
+  height: { xs: '100%' },
+  marginLeft: { xs: 0, md: '10%', lg: '30%' },
+  marginTop: '4rem',
+  boxShadow: theme => theme.shadows[13]
+};
+
+export const CreateAccountTitle: SxProps<Theme> = {
+  textAlign: 'center',
+  fontWeight: '700'
+};
+
+export const CreateAccountInputs: SxProps<Theme> = {
+  width: '100%',
+  margin: '.25rem 0',
+  '& .MuiInputBase-input': { p: '10px 12px' },
+  '& .MuiInputLabel-root': { top: '-5px' }
+};
