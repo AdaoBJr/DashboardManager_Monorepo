8d38972389b8cfd3a63c13e372815441e878dc0d
AdaoBJr <adaobjr@gmail.com>
2022-08-01 15:43:47 -0400

    refactor: :sparkles: created header on articles

---
+++ packages/app/src/articles/header.ts
@@ -0,0 +1,9 @@
+import { LinksProps } from '@dash/module-components';
+
+export const headerLinks: LinksProps = {
+  links: [
+    { name: 'Home', url: '/home' },
+    { name: 'Quem Somos', url: '/somos' },
+    { name: 'Contato', url: 'contato' }
+  ]
+};
