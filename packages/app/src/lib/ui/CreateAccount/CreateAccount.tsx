fedfaa32b499364cb11b0244b2bbad6c5ae5ee39
AdaoBJr <adaobjr@gmail.com>
2022-08-01 17:25:25 -0400

    refactor: :boom: updated CreateAccount on App

--- packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
+++ packages/app/src/lib/ui/CreateAccount/CreateAccount.tsx
@@ -1,24 +1,18 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React from 'react';
 import { Button, Grid } from '@mui/material';
+import { AnimationModule } from '@dash/module-components';
 import { CreateAccountModule } from '@dash/module-customer';
 
 import { Header } from 'lib/shared';
-import { createAccount } from 'articles';
+import { PaperForm } from './styles';
 import { ReactFC } from 'types/interface';
-import { CreateAccountDomain } from 'types/domain';
-import { useCreateCustomer } from 'services/infra/requests';
+import { useCreateAccount } from 'services/talons';
 import { Form, GridContainer, PaperFormWrapper } from 'lib/shared/__styles__';
 
 export const CreateAccount: React.FC<ReactFC> = () => {
-  const [values, setValues] = useState({} as CreateAccountDomain);
-  const { mutate, response } = useCreateCustomer({ values });
-
-  const handleBlur = (value: CreateAccountDomain) => setValues({ ...values, ...value });
-
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    mutate(values as any);
-  };
+  const {
+    compProps: { formProps, createAccountModuleProps, buttonProps, animationProps }
+  } = useCreateAccount();
 
   return (
     <GridContainer container>
@@ -26,21 +20,15 @@ export const CreateAccount: React.FC<ReactFC> = () => {
         <Header />
       </Grid>
       <Grid item md={6}>
-        <PaperFormWrapper>
-          <Form onSubmit={handleSubmit} autoComplete="off" noValidate>
-            <CreateAccountModule
-              container={{ spacing: 1 }}
-              input={{
-                sx: { width: '100%', '& .MuiInputBase-input': { padding: '10px 12px' } }
-              }}
-              articles={createAccount}
-              onBlur={handleBlur}
-            />
-            <Button type="submit" variant="contained" onClick={handleSubmit}>
-              Enviar
-            </Button>
+        <PaperFormWrapper sx={PaperForm}>
+          <Form {...formProps}>
+            <CreateAccountModule {...createAccountModuleProps} />
+            <Button {...buttonProps}>Enviar</Button>
           </Form>
         </PaperFormWrapper>
+      </Grid>
+      <Grid item md={6}>
+        <AnimationModule {...animationProps} />
       </Grid>
     </GridContainer>
   );
