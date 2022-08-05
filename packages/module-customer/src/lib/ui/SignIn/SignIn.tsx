a221312a2efa54602ea7bf8f0ccffa5b3c4e5331
AdaoBJr <adaobjr@gmail.com>
2022-08-05 15:50:23 -0400

    refactor: :zap: updated SignInModule - removed logics - using Formik

--- packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
+++ packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
@@ -1,46 +1,35 @@
-import React, { ChangeEvent, useCallback, useState } from 'react';
+import React from 'react';
+import { Field, ErrorMessage } from 'formik';
 import { Grid, Typography } from '@mui/material';
 import { InputText } from '@dash/module-components';
 
 import { SignInProps } from '../../../types/interface';
-import { SignInDomain } from '../../../types/domain';
 
 export const SignInModule: React.FC<SignInProps> = ({
   title,
   container,
   item,
   input,
-  articles,
-  onBlur
-}) => {
-  const [values, setValues] = useState({} as SignInDomain);
-
-  const handleChange = useCallback(
-    ({ target }: ChangeEvent<HTMLInputElement>) =>
-      setValues({ ...values, [target.name]: target.value }),
-    [setValues]
-  );
-
-  return (
-    <Grid container {...container}>
-      {title?.name && (
-        <Grid item xs={title.xs} sm={title.sm} {...title?.item}>
-          <Typography variant="h6" sx={{ ...title?.sx }} {...title?.text}>
-            {title.name}
-          </Typography>
-        </Grid>
-      )}
-      {articles?.map(data => (
-        <Grid key={data.id} item xs={data.xs} sm={data.sm} {...item}>
-          <InputText
-            {...data}
-            variant={input?.variant || 'outlined'}
-            onChange={handleChange}
-            onBlur={() => onBlur?.({ ...values })}
-            {...input}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  );
-};
+  articles
+}) => (
+  <Grid container {...container}>
+    {title?.name && (
+      <Grid item xs={title.xs} sm={title.sm} {...title?.item}>
+        <Typography variant="h6" sx={{ ...title?.sx }} {...title?.text}>
+          {title.name}
+        </Typography>
+      </Grid>
+    )}
+    {articles?.map(data => (
+      <Grid key={data.id} item xs={data.xs} sm={data.sm} {...item}>
+        <Field
+          as={InputText}
+          {...data}
+          variant={input?.variant || 'outlined'}
+          helperText={<ErrorMessage name={data.name} />}
+          {...input}
+        />
+      </Grid>
+    ))}
+  </Grid>
+);
