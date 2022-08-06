3e790019723b653d6494fea2cd59b115a3e424de
AdaoBJr <adaobjr@gmail.com>
2022-08-06 01:20:27 -0400

    refactor: :art: added talon useSignIn on SignInModule

--- packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
+++ packages/module-customer/src/lib/ui/SignIn/SignIn.tsx
@@ -4,6 +4,7 @@ import { InputText } from '@dash/module-components';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Grid, IconButton, InputAdornment, Typography } from '@mui/material';
 
+import { useSignIn } from '../../../services/talons';
 import { SignInProps } from '../../../types/interface';
 
 export const SignInModule: React.FC<SignInProps> = ({
@@ -13,36 +14,41 @@ export const SignInModule: React.FC<SignInProps> = ({
   input,
   articles,
   showPwd,
+  handleErrors,
   handleShowPassword
-}) => (
-  <Grid container {...container}>
-    {title?.name && (
-      <Grid item xs={title.xs} sm={title.sm} {...title?.item}>
-        <Typography variant="h6" sx={{ ...title?.sx }} {...title?.text}>
-          {title.name}
-        </Typography>
-      </Grid>
-    )}
-    {articles?.map(data => (
-      <Grid key={data.id} item xs={data.xs} sm={data.sm} {...item}>
-        <Field
-          as={InputText}
-          {...data}
-          type={showPwd || data.name !== 'password' ? 'text' : 'password'}
-          variant={input?.variant || 'outlined'}
-          helperText={<ErrorMessage name={data.name} />}
-          InputProps={{
-            endAdornment: data.name === 'password' && (
-              <InputAdornment position="end">
-                <IconButton onClick={handleShowPassword} edge="end">
-                  {showPwd ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          {...input}
-        />
-      </Grid>
-    ))}
-  </Grid>
-);
+}) => {
+  useSignIn({ handleErrors });
+
+  return (
+    <Grid container {...container}>
+      {title?.name && (
+        <Grid item xs={title.xs} sm={title.sm} {...title?.item}>
+          <Typography variant="h6" sx={{ ...title?.sx }} {...title?.text}>
+            {title.name}
+          </Typography>
+        </Grid>
+      )}
+      {articles?.map(data => (
+        <Grid key={data.id} item xs={data.xs} sm={data.sm} {...item}>
+          <Field
+            as={InputText}
+            {...data}
+            type={showPwd || data.name !== 'password' ? 'text' : 'password'}
+            variant={input?.variant || 'outlined'}
+            helperText={<ErrorMessage name={data.name} />}
+            InputProps={{
+              endAdornment: data.name === 'password' && (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    {showPwd ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            {...input}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
