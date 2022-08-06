5c67a98214796c0f6865176ec771f8ab10076c46
AdaoBJr <adaobjr@gmail.com>
2022-08-06 01:18:51 -0400

    refactor: :label: added useSignInProps on interfaces

--- packages/module-customer/src/types/interface/SignIn/index.ts
+++ packages/module-customer/src/types/interface/SignIn/index.ts
@@ -1,1 +1,18 @@
-export * from './SignInProps';
+import { ReactFC } from '../React';
+import { GridProps, TextFieldProps } from '@mui/material';
+import { InputArticles, TitleArticles } from '../../shared';
+
+export interface SignInProps extends ReactFC {
+  title?: TitleArticles;
+  container?: GridProps;
+  item?: GridProps;
+  input?: TextFieldProps;
+  articles?: InputArticles[];
+  showPwd?: boolean;
+  handleErrors: (error: boolean) => void;
+  handleShowPassword?: () => void;
+}
+
+export interface useSignInProps {
+  handleErrors: (error: boolean) => void;
+}
