0dd2022ece5fe3cc978706a95a099657f3f22131
AdaoBJr <adaobjr@gmail.com>
2022-08-06 22:28:41 -0400

    refactor: :label: updated interfaces on App/Types

--- packages/app/src/types/domain/index.ts
+++ packages/app/src/types/domain/index.ts
@@ -1,2 +1,18 @@
-export * from './CreateAccount';
-export * from './SignIn';
+export interface SignInDomain {
+  email: string;
+  password: string;
+}
+
+export interface CreateAccountDomain {
+  firstname: string;
+  lastname: string;
+  email: string;
+  dateOfBirth: string;
+  gender: number;
+  cpf: string;
+  country: string;
+  telephone: string;
+  subscribe: boolean;
+  password: string;
+  password_confirm: string;
+}
