086bab5de205bc09f36f599ca55e021e8cdb623a
AdaoBJr <adaobjr@gmail.com>
2022-08-06 22:30:08 -0400

    refactor: :art: created valuesRegisterInittial on types/shared

--- packages/app/src/types/shared/inittial/index.ts
+++ packages/app/src/types/shared/inittial/index.ts
@@ -2,3 +2,17 @@ export const valuesLoginInittial = {
   email: '',
   password: ''
 };
+
+export const valuesRegisterInittial = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  dateOfBirth: '',
+  gender: 0,
+  cpf: '',
+  country: '',
+  telephone: '',
+  subscribe: false,
+  password: '',
+  password_confirm: ''
+};
