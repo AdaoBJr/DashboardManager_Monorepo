b964ae9a6121450157bd455133041a79f2bcf7eb
AdaoBJr <adaobjr@gmail.com>
2022-08-06 22:29:10 -0400

    refactor: :label: updated interfaces on module-customer/types

--- packages/module-customer/src/types/interface/CreateAccount/index.ts
+++ packages/module-customer/src/types/interface/CreateAccount/index.ts
@@ -1,1 +1,15 @@
-export * from './CreateAccountProps';
+import { GridProps, TextFieldProps } from '@mui/material';
+import { InputArticles, TitleArticles } from '../../shared';
+import { ReactFC } from '../React';
+
+export interface CreateAccountProps extends ReactFC {
+  title?: TitleArticles;
+  container?: GridProps;
+  item?: GridProps;
+  input?: TextFieldProps;
+  articles?: InputArticles[];
+  handleErrors: (error: boolean) => void;
+}
+export interface useCreateAccountProps {
+  handleErrors: (error: boolean) => void;
+}
