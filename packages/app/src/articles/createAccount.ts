7ce32c8d33cee0f9c64ee43775e8b84cb4e0349e
AdaoBJr <adaobjr@gmail.com>
2022-08-11 23:30:08 -0400

    refactor: :art: updated createAccountInputs

--- packages/app/src/articles/createAccount.ts
+++ packages/app/src/articles/createAccount.ts
@@ -1,3 +1,6 @@
+import { valuesRegisterInittial } from 'types/shared';
+const { country, gender } = valuesRegisterInittial;
+
 export const createAccountTitle = {
   xs: 12,
   sm: undefined,
@@ -6,27 +9,87 @@ export const createAccountTitle = {
 };
 
 export const createAccountInputs = [
-  { xs: 12, sm: undefined, id: 'firstname', name: 'firstname', label: 'Nome' },
-  { xs: 12, sm: undefined, id: 'lastname', name: 'lastname', label: 'Sobrenome' },
-  { xs: 12, sm: undefined, id: 'email', name: 'email', label: 'E-mail' },
+  {
+    xs: 12,
+    sm: undefined,
+    id: 'firstname',
+    name: 'firstname',
+    label: 'Nome',
+    select: false
+  },
+  {
+    xs: 12,
+    sm: undefined,
+    id: 'lastname',
+    name: 'lastname',
+    label: 'Sobrenome',
+    select: false
+  },
+  {
+    xs: 12,
+    sm: undefined,
+    id: 'email',
+    name: 'email',
+    label: 'E-mail',
+    select: false
+  },
   {
     xs: 12,
     sm: 6,
     id: 'dateOfBirth',
     name: 'dateOfBirth',
-    label: 'Data de aniversário'
+    label: 'Data de aniversário',
+    select: false
   },
-  { xs: 12, sm: 6, id: 'gender', name: 'gender', label: 'Gênero' },
-  { xs: 12, sm: 6, id: 'cpf', name: 'cpf', label: 'CPF' },
-  { xs: 12, sm: 6, id: 'country', name: 'country', label: 'País' },
-  { xs: 12, sm: 6, id: 'telephone', name: 'telephone', label: 'Telefone' },
-  { xs: 12, sm: 6, id: 'subscribe', name: 'subscribe', label: 'Se Inscreva' },
-  { xs: 12, sm: undefined, id: 'password', name: 'password', label: 'Senha' },
+  {
+    xs: 12,
+    sm: 6,
+    id: 'gender',
+    name: 'gender',
+    label: 'Selecione um Gênero:',
+    select: true,
+    inittial: gender
+  },
+  { xs: 12, sm: 6, id: 'cpf', name: 'cpf', label: 'CPF', select: false },
+  {
+    xs: 12,
+    sm: 6,
+    id: 'country',
+    name: 'country',
+    label: 'Selecione um País:',
+    select: true,
+    inittial: country
+  },
+  {
+    xs: 12,
+    sm: 6,
+    id: 'telephone',
+    name: 'telephone',
+    label: 'Telefone',
+    select: false
+  },
+  {
+    xs: 12,
+    sm: 6,
+    id: 'subscribe',
+    name: 'subscribe',
+    label: 'Se Inscreva',
+    select: false
+  },
+  {
+    xs: 12,
+    sm: undefined,
+    id: 'password',
+    name: 'password',
+    label: 'Senha',
+    select: false
+  },
   {
     xs: 12,
     sm: undefined,
     id: 'password_confirm',
     name: 'password_confirm',
-    label: 'Confirme a senha'
+    label: 'Confirme a senha',
+    select: false
   }
 ];
