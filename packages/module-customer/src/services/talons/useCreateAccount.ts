5e19b2b637d1865f3b6a3d107bc62dda67aaf76b
AdaoBJr <adaobjr@gmail.com>
2022-08-16 19:30:43 -0400

    refactor: :fire: removed code not used

--- packages/module-customer/src/services/talons/useCreateAccount.ts
+++ packages/module-customer/src/services/talons/useCreateAccount.ts
@@ -1,25 +1,10 @@
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useFormikContext } from 'formik';
 
 import { useCreateAccountProps } from '../../types/interface';
 
 export const useCreateAccount = (props: useCreateAccountProps) => {
-  const { values: inputValues, errors } = useFormikContext();
-
-  const { gender, country } = inputValues as any;
-  const dropdown = { gender: gender[0], country: country[0] };
-  const [dropValues, setDropdownValues] = useState<any>(dropdown);
-
-  const isDate = useCallback(
-    (name: string, id: string) => name.includes('date') || id.includes('date'),
-    []
-  );
-
-  const handleDropChange = useCallback(
-    ({ target: { name, value } }: ChangeEvent<HTMLInputElement>) =>
-      setDropdownValues({ ...dropValues, [name]: value }),
-    [dropValues, setDropdownValues]
-  );
+  const { errors } = useFormikContext();
 
   const formErrors = useCallback(() => {
     const isError = Object.values(errors).length > 0;
@@ -28,5 +13,5 @@ export const useCreateAccount = (props: useCreateAccountProps) => {
 
   useEffect(formErrors, [errors]);
 
-  return { dropValues, isDate, handleDropChange };
+  return {};
 };
