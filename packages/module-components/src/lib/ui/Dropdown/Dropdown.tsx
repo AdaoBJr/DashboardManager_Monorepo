36d4d415651f16bb44605090123fce06097905d4
AdaoBJr <adaobjr@gmail.com>
2022-08-16 15:11:47 -0400

    feat: :boom: created Dropdown on module-components

---
+++ packages/module-components/src/lib/ui/Dropdown/Dropdown.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent, useCallback, useMemo } from 'react';
+import { MenuItem, TextField, TextFieldProps } from '@mui/material';
+import { ErrorMessage, useField, useFormikContext } from 'formik';
+
+import { DropdownModuleProps } from '../../../types/interface';
+
+export const DropdownModule: React.FC<DropdownModuleProps> = props => {
+  const { input, data, ...rest } = props;
+  const [field, meta] = useField(data.name);
+  const { setFieldValue } = useFormikContext();
+
+  const handleChange = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) =>
+      setFieldValue(data.name, value),
+    [data.name, setFieldValue]
+  );
+
+  const dropdownProps = useMemo<TextFieldProps>(
+    () => ({
+      ...field,
+      ...data,
+      ...input,
+      ...rest,
+      select: true,
+      fullWidth: true,
+      variant: input?.variant || 'outlined',
+      helperText: <ErrorMessage name={data.name} />,
+      onChange: handleChange
+    }),
+    [props, field]
+  );
+
+  if (meta && meta.touched && meta.error) {
+    dropdownProps.error = true;
+    dropdownProps.helperText = meta.error;
+  }
+
+  return (
+    <TextField {...dropdownProps}>
+      {data.options?.map(option => (
+        <MenuItem key={option} value={option}>
+          {option}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
