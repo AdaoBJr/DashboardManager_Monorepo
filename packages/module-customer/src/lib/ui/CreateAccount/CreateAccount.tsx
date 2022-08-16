5adf9c6393022ae9e5cd2ebbfac947deb5460eea
AdaoBJr <adaobjr@gmail.com>
2022-08-16 15:15:59 -0400

    refactor: :zap: updated CreateAccount on module-customer

--- packages/module-customer/src/lib/ui/CreateAccount/CreateAccount.tsx
+++ packages/module-customer/src/lib/ui/CreateAccount/CreateAccount.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { ErrorMessage, Field } from 'formik';
-import { Grid, MenuItem, Typography } from '@mui/material';
-import { InputText } from '@dash/module-components';
+import { Grid, Typography } from '@mui/material';
+import { renderComp } from '@dash/module-components';
 
 import { useCreateAccount } from '../../../services/talons';
 import { CreateAccountProps } from '../../../types/interface';
@@ -14,9 +13,7 @@ export const CreateAccountModule: React.FC<CreateAccountProps> = ({
   articles,
   handleErrors
 }) => {
-  const { dropValues, isDate, handleDropChange } = useCreateAccount({
-    handleErrors
-  });
+  useCreateAccount({ handleErrors });
 
   return (
     <Grid container {...container}>
@@ -29,35 +26,7 @@ export const CreateAccountModule: React.FC<CreateAccountProps> = ({
       )}
       {articles?.map(data => (
         <Grid key={data.id} item xs={data.xs} sm={data.sm} {...item}>
-          {data.select && data.inittial ? (
-            <Field
-              as={InputText}
-              value={dropValues[data.name]}
-              type={isDate(data.name, data.id) ? 'date' : 'text'}
-              InputLabelProps={isDate(data.name, data.id) ? { shrink: true } : {}}
-              {...data}
-              variant={input?.variant || 'outlined'}
-              helperText={<ErrorMessage name={data.name} />}
-              onChange={handleDropChange}
-              {...input}
-            >
-              {data.inittial?.map(item => (
-                <MenuItem key={item} value={item}>
-                  {item}
-                </MenuItem>
-              ))}
-            </Field>
-          ) : (
-            <Field
-              as={InputText}
-              type={isDate(data.name, data.id) ? 'date' : 'text'}
-              InputLabelProps={isDate(data.name, data.id) ? { shrink: true } : {}}
-              {...data}
-              variant={input?.variant || 'outlined'}
-              helperText={<ErrorMessage name={data.name} />}
-              {...input}
-            />
-          )}
+          {renderComp(data.type, { data, input })}
         </Grid>
       ))}
     </Grid>
