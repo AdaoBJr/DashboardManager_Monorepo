7bd4f784bfa9df695aaf66425d3bc395a5925135
AdaoBJr <adaobjr@gmail.com>
2022-08-16 23:43:19 -0400

    refactor: :fire: removed code not used

--- packages/module-components/src/lib/ui/InputText/InputText.tsx
+++ packages/module-components/src/lib/ui/InputText/InputText.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TextField } from '@mui/material';
-import { ErrorMessage, Field } from 'formik';
+import { Field } from 'formik';
 
 import { InputTextModuleProps } from '../../../types/interface';
 
