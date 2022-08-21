42d8c16a690bbc15ddfc96436319d4bbf215f2b4
AdaoBJr <adaobjr@gmail.com>
2022-08-21 19:52:07 -0400

    refactor: :fire: removed InputPhone on module-components

--- packages/module-components/src/lib/ui/InputPhone/InputPhone.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Field } from 'formik';
-import { TextField } from '@mui/material';
-
-import { InputPhoneModuleProps } from '../../../types/interface';
-
-export const InputPhoneModule: React.FC<InputPhoneModuleProps> = props => {
-  return <Field as={TextField} {...props.data} {...props.input} />;
-};
--- packages/module-components/src/lib/ui/index.ts
+++ packages/module-components/src/lib/ui/index.ts
@@ -3,5 +3,4 @@ export * from './Animation';
 export * from './InputText';
 export * from './InputDate';
 export * from './InputCheck';
-export * from './InputPhone';
 export * from './Dropdown';
