102d11d4e0bfc28c5f396f52f3edfdc87267a173
AdaoBJr <adaobjr@gmail.com>
2022-09-18 19:19:49 -0400

    refactor: :boom: added InputFile on ButtonUpload

--- packages/module-components/src/lib/ui/ButtonUpload/ButtonUpload.tsx
+++ packages/module-components/src/lib/ui/ButtonUpload/ButtonUpload.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
-import { ButtonUploadProps } from '../../../types/interface/ButtonUpload';
+import { ButtonUploadProps } from '../../../types/interface';
+import { InputFile } from '../../shared';
 
 export const ButtonUpload: React.FC<ButtonUploadProps> = props => (
-  <Button variant={props.muiProps.variant || 'contained'} fullWidth {...props.muiProps}>
-    <label style={{ cursor: 'pointer' }}>
-      {props.children || 'Enviar Arquivo'}
-      <input type="file" hidden />
-    </label>
+  <Button
+    variant={props.muiProps.variant || 'contained'}
+    fullWidth
+    {...props.muiProps}
+    startIcon={
+      props.muiProps?.startIcon && (
+        <InputFile
+          muiProps={{
+            sx: { marginTop: '5px', marginLeft: '12px', ...props.compProps?.startIconCSS }
+          }}
+        >
+          {props.muiProps?.startIcon}
+        </InputFile>
+      )
+    }
+    endIcon={
+      props.muiProps?.endIcon && (
+        <InputFile
+          muiProps={{
+            sx: { marginTop: '5px', marginRight: '12px', ...props.compProps?.endIconCSS }
+          }}
+        >
+          {props.muiProps?.endIcon}
+        </InputFile>
+      )
+    }
+    sx={{
+      ...props?.muiProps.sx,
+      p: 0,
+      height: props?.compProps?.height || '35px',
+      width: props?.compProps?.width || '185px'
+    }}
+  >
+    <InputFile />
   </Button>
 );
--- packages/module-components/src/types/interface/ButtonUpload/index.ts
+++ packages/module-components/src/types/interface/ButtonUpload/index.ts
@@ -1,8 +1,13 @@
+import { SxProps, Theme } from '@mui/material';
 import { ButtonComponent } from '../../shared';
 
 interface ButtonUpload {
   id?: string;
   text?: string;
+  height?: string;
+  width?: string;
+  startIconCSS?: SxProps<Theme>;
+  endIconCSS?: SxProps<Theme>;
 }
 
 export interface ButtonUploadProps extends ButtonComponent {
