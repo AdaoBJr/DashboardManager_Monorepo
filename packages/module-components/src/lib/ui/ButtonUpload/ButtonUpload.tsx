aa7605b5fb0e0b36fc776045f44a2703628f759a
AdaoBJr <adaobjr@gmail.com>
2022-09-18 23:05:38 -0400

    feat: :art: added talon useInputFile on ButtonUpload

--- packages/module-components/src/lib/ui/ButtonUpload/ButtonUpload.tsx
+++ packages/module-components/src/lib/ui/ButtonUpload/ButtonUpload.tsx
@@ -1,43 +1,58 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
-import { ButtonUploadProps } from '../../../types/interface';
 import { InputFile } from '../../shared';
+import { ButtonUploadProps } from '../../../types/interface';
 
-export const ButtonUpload: React.FC<ButtonUploadProps> = props => (
-  <Button
-    variant={props.muiProps.variant || 'contained'}
-    fullWidth
-    {...props.muiProps}
-    startIcon={
-      props.muiProps?.startIcon && (
-        <InputFile
-          muiProps={{
-            sx: { marginTop: '5px', marginLeft: '12px', ...props.compProps?.startIconCSS }
-          }}
-        >
-          {props.muiProps?.startIcon}
-        </InputFile>
-      )
-    }
-    endIcon={
-      props.muiProps?.endIcon && (
-        <InputFile
-          muiProps={{
-            sx: { marginTop: '5px', marginRight: '12px', ...props.compProps?.endIconCSS }
-          }}
-        >
-          {props.muiProps?.endIcon}
-        </InputFile>
-      )
-    }
-    sx={{
-      ...props?.muiProps.sx,
-      p: 0,
-      height: props?.compProps?.height || '35px',
-      width: props?.compProps?.width || '185px'
-    }}
-  >
-    <InputFile />
-  </Button>
-);
+export const ButtonUpload: React.FC<ButtonUploadProps> = props => {
+  const loadedFile = (file: string) => {
+    console.log('file', file);
+  };
+  return (
+    <Button
+      variant={props.muiProps.variant || 'contained'}
+      fullWidth
+      {...props.muiProps}
+      startIcon={
+        props.muiProps?.startIcon && (
+          <InputFile
+            muiProps={{
+              sx: {
+                marginTop: '5px',
+                marginLeft: '12px',
+                ...props.compProps?.startIconCSS
+              }
+            }}
+            compProps={{ loadedFile }}
+          >
+            {props.muiProps?.startIcon}
+          </InputFile>
+        )
+      }
+      endIcon={
+        props.muiProps?.endIcon && (
+          <InputFile
+            muiProps={{
+              sx: {
+                marginTop: '5px',
+                marginRight: '12px',
+                ...props.compProps?.endIconCSS
+              }
+            }}
+            compProps={{ loadedFile }}
+          >
+            {props.muiProps?.endIcon}
+          </InputFile>
+        )
+      }
+      sx={{
+        ...props?.muiProps.sx,
+        p: 0,
+        height: props?.compProps?.height || '35px',
+        width: props?.compProps?.width || '185px'
+      }}
+    >
+      <InputFile compProps={{ loadedFile }} />
+    </Button>
+  );
+};
