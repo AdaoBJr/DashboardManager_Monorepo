3a44fcd50c7336f751ff8b1dd19111378887ceec
AdaoBJr <adaobjr@gmail.com>
2022-09-19 22:29:57 -0400

    feat: :sparkles: created link into uploaded file on DocsUpload

--- packages/apps/gestao-nfs/src/lib/ui/DocsUpload/DocsUpload.tsx
+++ packages/apps/gestao-nfs/src/lib/ui/DocsUpload/DocsUpload.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Grid, Paper } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+import { Button, Grid, Paper } from '@mui/material';
 import { FileUploadOutlined } from '@mui/icons-material';
 import { Title, Body, ButtonUpload } from '@dash/module-components';
 
+const redirectUrl =
+  'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
 export const DocsUpload: React.FC = () => {
+  const [status, setStatus] = useState(false);
+
+  const handleViewFile = useCallback(() => {
+    if (status) window.open(redirectUrl, '_blank');
+    return null;
+  }, [status]);
+
   return (
     <Grid
       container
@@ -18,16 +27,25 @@ export const DocsUpload: React.FC = () => {
               muiProps={{ variant: 'h6', textAlign: 'center' }}
               compProps={{ text: 'NF' }}
             />
-            <Body
-              muiProps={{ variant: 'body1', sx: { mt: '5px' } }}
-              compProps={{ text: 'Materiais' }}
-            />
+            <div aria-hidden onClick={handleViewFile}>
+              <Body
+                muiProps={{
+                  variant: 'body1',
+                  sx: {
+                    mt: '5px',
+                    textDecoration: status ? 'underline' : 'inherit',
+                    cursor: status ? 'pointer' : 'inherit'
+                  }
+                }}
+                compProps={{ text: 'Materiais' }}
+              />
+            </div>
           </Grid>
           <Grid item xs={6} sm={4}>
             <Title muiProps={{ variant: 'h6' }} compProps={{ text: 'STATUS' }} />
             <Body
               muiProps={{ variant: 'body1', sx: { mt: '5px' } }}
-              compProps={{ text: 'PENDENTE' }}
+              compProps={{ text: status ? 'Concluído' : 'Pendente' }}
             />
           </Grid>
           <Grid item xs={12} sm={4} sx={{ mt: { xs: '1.625rem', sm: 0 } }}>
@@ -38,6 +56,7 @@ export const DocsUpload: React.FC = () => {
                 startIcon: <FileUploadOutlined />
               }}
             />
+            <Button onClick={() => setStatus(prev => !prev)}>Switch Status</Button>
           </Grid>
         </Grid>
       </Paper>
