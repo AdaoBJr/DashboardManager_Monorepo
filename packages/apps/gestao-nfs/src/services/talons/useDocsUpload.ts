41eced4a7a23c8e116a9d2ff7ebc19b64268f1df
AdaoBJr <adaobjr@gmail.com>
2022-09-25 18:22:13 -0400

    feat: :boom: added allowedFileSize on useDocsUpload

--- packages/apps/gestao-nfs/src/services/talons/useDocsUpload.ts
+++ packages/apps/gestao-nfs/src/services/talons/useDocsUpload.ts
@@ -8,13 +8,22 @@ export const useDocsUpload = () => {
   const params = useParams<{ contractId: string }>();
   const { getNFsData, setUploadNF } = useBFFNFs();
   const { data } = useQuery('nfUpload', async () => getNFsData(params.contractId!));
-  // const { mutate } = useMutation(data => setUploadNF(params.contractId!, data));
+  const { mutate } = useMutation(
+    async (data: { nome: string; arquivo: string }) =>
+      await setUploadNF(params.contractId!, data)
+  );
 
   const res = useMemo(() => data?.data, [data]);
 
-  const handleLoadFile = useCallback((file: File, file64: string | ArrayBuffer) => {
-    console.log('fileTalon'), console.log(file);
-    console.log(file64);
+  const handleLoadFile = useCallback(async (file: File, file64: string | ArrayBuffer) => {
+    const allowedFileSize = 5 * 1024 * 1024;
+    if (file.size > allowedFileSize) {
+      console.log('Tamanho máximo permitido 5MB');
+    } else {
+      const data = { nome: file.name, arquivo: String(file64) };
+      mutate(data);
+    }
   }, []);
+
   return { res, handleLoadFile };
 };
