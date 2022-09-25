bda25952f6445046408a1b02b92459f4ae6148a2
AdaoBJr <adaobjr@gmail.com>
2022-09-25 09:26:34 -0400

    feat: :clown_face: updated mocks on miragejs

--- src/services/infra/api/mirage/index.ts
+++ src/services/infra/api/mirage/index.ts
@@ -1,31 +1,59 @@
-import * as faker from 'faker';
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Model } from 'miragejs';
+import { v4 as randomId } from 'uuid';
+import { ContratoDocumentos } from '@dash/module-domain';
 
 import { Customer } from 'types/interface';
+
+/* istanbul ignore next */
+const mock_url_file =
+  'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+
+const docs_data = {
+  contrato: { id_contrato: '12345', cliente: 'John Doe', placa: 'HTJ-7654' },
+  lista_documentos: [
+    {
+      id: '000123',
+      nome: 'CNH',
+      scan_impressora: false,
+      status: 'pendente',
+      documento: { id_gad: null, nome: null, arquivo: null }
+    },
+    {
+      id: '000124',
+      nome: 'CRV',
+      scan_impressora: false,
+      status: 'concluído',
+      documento: { id_gad: null, nome: null, arquivo: mock_url_file }
+    },
+    {
+      id: '000125',
+      nome: 'Contrato',
+      scan_impressora: true,
+      status: 'concluído',
+      documento: { id_gad: null, nome: null, arquivo: mock_url_file }
+    },
+    {
+      id: '000126',
+      nome: 'Maxigarantia',
+      scan_impressora: true,
+      status: 'pendente',
+      documento: { id_gad: null, nome: null, arquivo: null }
+    }
+  ]
+};
 
 export const makeServer = () => {
   const server = createServer({
     models: {
-      customer: Model.extend<Partial<Customer>>({})
+      customer: Model.extend<Partial<Customer>>({}),
+      documents: Model.extend<Partial<ContratoDocumentos[]>>([])
     },
 
-    // factories: {
-    //   customer: Factory.extend({
-    //     firstname(i) {
-    //       return faker.name.firstName();
-    //     },
-    //     lastname(i) {
-    //       return faker.name.lastName();
-    //     },
-    //     email() {
-    //       return faker.internet.email().toLowerCase();
-    //     },
-    //   }),
-    // },
-
-    // seeds(server) {
-    //   server.createList('customer', 10);
-    // },
+    seeds(server) {
+      server.db.loadData({
+        documents: { ...docs_data }
+      });
+    },
 
     routes() {
       this.namespace = 'api';
@@ -46,6 +74,30 @@ export const makeServer = () => {
       });
       this.delete('/login', (schema): any => {
         return schema.db.dump();
+      });
+
+      this.post('/documento/:id', (_, request) => {
+        const id = request.params.id;
+        const req = JSON.parse(request.requestBody);
+        const doc_db = server.db.documents.update(1, {
+          lista_documentos: [
+            ...docs_data.lista_documentos.map(doc => {
+              if (doc.id === id) {
+                return {
+                  ...doc,
+                  status: 'concluído',
+                  documento: {
+                    arquivo: mock_url_file,
+                    nome: req.nome,
+                    id_gad: randomId() // esse id deverá ser do GAD
+                  }
+                };
+              }
+              return doc;
+            })
+          ]
+        });
+        return doc_db;
       });
     }
   });
